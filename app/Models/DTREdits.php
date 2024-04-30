16b8c44758a878ef4e63d6d977ce1282e508f375
MISVIS <sramisvis@gmail.com>
2024-04-30 16:30:26 +0800

    boot on edits

--- app/Models/DTREdits.php
+++ app/Models/DTREdits.php
@@ -9,6 +9,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DTREdits extends Model
 {
+    public static function boot(){
+        parent::boot();
+        static::updating(function($a){
+            $a->user_updated = \Auth::user()->user_id;;
+            $a->updated_at = \Carbon::now();
+        });
+
+        static::creating(function ($a){
+            $a->user_created = \Auth::user()->user_id;
+            $a->created_at = \Carbon::now();
+        });
+    }
     protected $table = 'hr_dtr_edits';
     protected $fillable = ['slug','employee_no','biometric_user_id','time'];
 
